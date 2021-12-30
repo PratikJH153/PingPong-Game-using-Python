76bd1b93c4414c25eb97a614f61d70c87ac2593e
PratikJH <pratik.jh2017@gmail.com>
2021-12-30 14:44:49 +0530

    Added Player1 and Player2 Paddle with functionality

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("HELLO")
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=900, height=600)
+screen.bgcolor("black")
+
+screen.tracer(0)
+
+player1 = Paddle(410)
+player2 = Paddle(-410)
+
+screen.listen()
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    screen.onkeypress(key="Up", fun=player1.up)
+    screen.onkeypress(key="Down", fun=player1.down)
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, x_position):
+        super().__init__()
+        self.speed("fastest")
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=4, stretch_len=1)
+        self.goto(x_position, 0)
+
+    def up(self):
+        if self.ycor() < 240:
+            previous_y = self.ycor()
+            self.sety(previous_y + 15)
+
+    def down(self):
+        if self.ycor() > -240:
+            previous_y = self.ycor()
+            self.sety(previous_y - 15)
